2dff21062d032f5ae7d848bc20cddadfee0ac37d
Wolfgang <Wolfgang@macbookpros-macbook.local>
2018-02-21 13:45:25 -0600

    Initial Commit

---
+++ Cree Conjugation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cree Conjugation
+//
+//  Created by Wolfgang on 2018-02-21.
+//  Copyright © 2018 nistosap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
